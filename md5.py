cec6266f66fbd0028ab9e7276c2b4e02ee923134
Mkali07 <45468389+Mkali07@users.noreply.github.com>
2018-12-01 13:21:55 +0330

    Update md5.py

--- md5.py
+++ md5.py
@@ -4,6 +4,7 @@
 
 import re,requests,random,threading,sys,json,time
 from termcolor import colored
+from os import system
 
 try:
     from termcolor import colored
@@ -13,12 +14,28 @@ except:
 
 #####End import modules######
 
+
+
+
 class view_info(object):
     def __init__(self):
+    	self.cls()
+
+	if len(sys.argv) != 3:
+		self.cls()
+		self.help()
+		sys.exit()
+
+	self.banner()
 
         global hashvalue,hashtype,flag
-        self.hashvalue = list(open(sys.argv[1],'r').read().splitlines())
-        self.hashtype = sys.argv[2]
+	try:
+        	self.hashvalue = list(open(sys.argv[1],'r').read().splitlines())
+        	self.hashtype = sys.argv[2]
+	except:
+		self.cls()
+		self.help()
+		sys.exit()
 
     	self.user_agents = ["Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko)",
@@ -43,11 +60,10 @@ class view_info(object):
         cracked = []
         try:
             for hash in self.hashvalue:
-		    if hash.strip():
-                    	run = threading.Thread(target=self.md5_1, args=(hash, self.hashtype))
-                    	run.start()
-                    	thread.append(run)
-                    	time.sleep(0.05)
+                    run = threading.Thread(target=self.md5_1, args=(hash, self.hashtype))
+                    run.start()
+                    thread.append(run)
+                    time.sleep(0.05)
             for j in thread:
                     j.join()
         except:
@@ -56,24 +72,6 @@ class view_info(object):
             with open('faild_crack_md5.txt', 'a') as notmd5:
                     notmd5.write(hashvalue+'\n')
             pass
-
-
-    def Auxiliary_1(self,hashvalue,hashtype):
-	url = "https://md5.gromweb.com/?md5="+hashvalue
-        response = requests.get(url,headers=self.head,timeout=20)
-	if "was succesfully reversed into the string:" not in response.text:
-		self.md5_3(hashvalue,self.hashtype)
-	else:
-        	match = re.search(r'<em class="long-content string">(.*?)</em></p>', response.text).group(1)
-        	if match:
-            		flag = True
-	    		print (colored("[+]",'blue')),("try Crack hash  %s Cracked" % hashvalue)
-            		with open('crack_md5.txt', 'a') as cmd5:
-                    		cmd5.write(hashvalue+":"+match+'\n')
-        	else:
-            		flag = False
-            		self.md5_4(hashvalue,self.hashtype)
-
 
 
 
@@ -95,17 +93,14 @@ class view_info(object):
     def md5_2(self,hashvalue,hashtype):
         self.flag = False
         response = requests.get("http://www.nitrxgen.net/md5db/"+hashvalue,headers=self.head).text
-	if hashvalue == "":
-		self.md5_3(hashvalue,self.hashtype)
         if response:
             flag = True
 	    print (colored("[+]",'blue')),("try Crack hash  %s Cracked" % hashvalue)
             with open('crack_md5.txt', 'a') as cmd5:
                     cmd5.write(hashvalue+":"+response+'\n')
         else:
-	    self.md5_3(hashvalue,self.hashtype)
             flag = False
-            
+            self.md5_3(hashvalue,self.hashtype)
 
 
     def md5_3(self,hashvalue,hashtype):
@@ -113,6 +108,7 @@ class view_info(object):
         response = requests.get(url,headers=self.head,timeout=120)
 	if "No hashes found for" in response.text:
 		self.md5_4(hashvalue,self.hashtype)
+
 	else:
         	match = re.findall('<span title="decrypted md5 hash">(.*)</span>', response.text.encode('utf-8'))[0]
         	if match:
@@ -146,13 +142,33 @@ class view_info(object):
                     cmd5.write(hashvalue+":"+response+'\n')
         else:
             flag = False
-	    print (colored("[+]",'blue')),("try Crack hash  %s Cracked" % hashvalue)
             with open('faild_crack_md5.txt', 'a') as notmd5:
                     notmd5.write(hashvalue+'\n')
 
+    def cls(self):
+	if sys.platform =='win32':
+        	system("cls")
+	else:
+        	system("clear")
 
+    def banner(self):
+	print '''
+	====================
+	Crack Md5 hashes list
 
+	By Mkali07
+
+	
+	===================
+
+	'''
+    
+    def help(self):
+	print '''
+
+	ex: python md5.py hashes.txt md5
+	
+	'''
 
 
 view_info()
-
